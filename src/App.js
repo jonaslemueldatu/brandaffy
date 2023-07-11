55a3ae9e3566a5f3df8dfac2eb3dcf45e825b2a3
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-11 20:28:01 +0800

    ASd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-simport './App.css';
+import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Affiliatelandingpage from "./pages/Affiliatelandingpage"
 import Affiliateregistrationpage from "./pages/Affiliateregistrationpage"
@@ -10,8 +10,8 @@ import Dashboardbrandprofile from './pages/Dashboardbrandprofile'
 import Dashboardbrandinfluencerhub from './pages/Dashboardbrandinfluencerhub';
 import Dashboardbrandinfluencerbox from './pages/Dashboardbrandinfluencerbox';
 import Dashboardaffiliateprofile from './pages/Dashboardaffiliateprofile';
-import Dashboardaffiliateinfluencerhub from './pages/Dashboardaffiliateinfluencerhub'
-import Dashboardaffiliatesettings from './pages/Dashboardaffiliatesettings'
+import Dashboardaffiliateinfluencerhub from './pages/Dashboardaffiliateinfluencerhub';
+import Dashboardaffiliatesettings from './pages/Dashboardaffiliatesettings';
 import { RequireAuth } from 'react-auth-kit';
 
 
@@ -27,13 +27,13 @@ function App() {
         <Route path="/brand/register" element={<Brandregistrationpage />} />
         <Route path="/affiliate/register" element={<Affiliateregistrationpage />} />
         <Route path="/affiliate/login" element={<Affiliateloginpage />} />
-        <Route path="/dashboard/brand/profile" element={<RequireAuth loginPath='/brand/login'><Dashboardbrandprofile /></RequireAuth>}/>
-        <Route path="/dashboard/brand/influencerhub" element={<RequireAuth loginPath='/brand/login'><Dashboardbrandinfluencerhub /></RequireAuth>}/>
-        <Route path="/dashboard/brand/influencerbox" element={<RequireAuth loginPath='/brand/login'><Dashboardbrandinfluencerbox /></RequireAuth>}/>
-        <Route path="/dashboard/affiliate/profile" element={<RequireAuth loginPath='/affiliate/login'><Dashboardaffiliateprofile /></RequireAuth>}/>
-        <Route path="/dashboard/affiliate/profile/:id" element={<RequireAuth loginPath='/affiliate/login'><Dashboardaffiliateprofile /></RequireAuth>}/>
-        <Route path="/dashboard/affiliate/influencerhub" element={<RequireAuth loginPath='/affiliate/login'><Dashboardaffiliateinfluencerhub /></RequireAuth>}/>
-        <Route path="/dashboard/affiliate/settings/:subsection" element={<RequireAuth loginPath='/affiliate/login'><Dashboardaffiliatesettings /></RequireAuth>}/>
+        <Route path="/dashboard/brand/profile" element={<RequireAuth loginPath='/brand/login'><Dashboardbrandprofile /></RequireAuth>} />
+        <Route path="/dashboard/brand/influencerhub" element={<RequireAuth loginPath='/brand/login'><Dashboardbrandinfluencerhub /></RequireAuth>} />
+        <Route path="/dashboard/brand/influencerbox" element={<RequireAuth loginPath='/brand/login'><Dashboardbrandinfluencerbox /></RequireAuth>} />
+        <Route path="/dashboard/affiliate/profile" element={<RequireAuth loginPath='/affiliate/login'><Dashboardaffiliateprofile /></RequireAuth>} />
+        <Route path="/dashboard/affiliate/profile/:id" element={<RequireAuth loginPath='/affiliate/login'><Dashboardaffiliateprofile /></RequireAuth>} />
+        <Route path="/dashboard/affiliate/influencerhub" element={<RequireAuth loginPath='/affiliate/login'><Dashboardaffiliateinfluencerhub /></RequireAuth>} />
+        <Route path="/dashboard/affiliate/settings/:subsection" element={<RequireAuth loginPath='/affiliate/login'><Dashboardaffiliatesettings /></RequireAuth>} />
       </Routes>
     </div >
   );
